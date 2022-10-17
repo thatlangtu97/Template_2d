50be06294aed6693020ffb6a14a46530219d42fc
Toan <toantrongluong@gmail.com>
2022-10-18 01:18:29 +0700

    commit va cham effector

---
+++ Assets/Game/Scripts/CoreGame/GamePlay/Platform/DetecPlatformEffector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Core.GamePlay;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class DetecPlatformEffector : MonoBehaviour
+{
+    [ChildGameObjectsOnly]
+    public BoxCollider2D Collider2D;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    // Check Collision When Use Platform Effector
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.position.y > transform.position.y + Collider2D.offset.y)
+        {
+            ComponentManager component = other.gameObject.GetComponent<ComponentManager>();
+            if (component)
+            {
+                component.stateMachine.ChangeState(NameState.JumpState,true);
+            }
+        }
+    }
+}
