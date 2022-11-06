3cc09daee9201590c2dce749d10e2a2a40dec271
Toan <toantrongluong@gmail.com>
2022-11-06 18:19:32 +0700

    commit Room

--- Assets/Game/Scripts/CoreGame/GamePlay/RoomController/RoomController.cs
+++ Assets/Game/Scripts/CoreGame/GamePlay/RoomController/RoomController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Core.GamePlay;
@@ -5,15 +6,69 @@ using UnityEngine;
 
 public class RoomController : MonoBehaviour
 {
-    public EventCollection eventRoom;
+    public EventCollection eventCollectionData;
+    protected Dictionary<int, IComboEvent> idEventTrigged = new Dictionary<int, IComboEvent>();
+    public float timeTrigger;
+    public GameObject Boss;
+    public float timeBoss;
+    public bool started;
+    public bool Entry;
+    private void Awake()
+    {
+        this.RegisterListener(EventID.ENTRY_ROOM, (sender, param) => OnEntryRoom());
+        this.RegisterListener(EventID.OPEN_DOOR, (sender, param) => FinishRoom());
+    }
+
+    private void FinishRoom()
+    { 
+        Entry = false;
+        foreach (IComboEvent temp in idEventTrigged.Values)
+        {
+            temp.Recycle();
+        }
+    }
+
+    void OnEntryRoom()
+    {
+        Entry = true;
+    }
     void Start()
     {
         
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!Entry) return;
+        timeTrigger += Time.deltaTime;
+
+        if (eventCollectionData.EventCombo != null)
+        {
+            foreach (IComboEvent tempComboEvent in eventCollectionData.EventCombo)
+            {
+                if (timeTrigger > tempComboEvent.timeTrigger )
+                {
+                    if (!idEventTrigged.ContainsKey(tempComboEvent.id))
+                    {
+                        tempComboEvent.OnEventTrigger(null);
+                        idEventTrigged.Add(tempComboEvent.id, tempComboEvent);
+                    }
+                    else
+                    {
+                        tempComboEvent.OnUpdateTrigger();
+                    }
+                }
+            }
+        }
+
+        if (timeTrigger > timeBoss && !started)
+        {
+            Boss.SetActive(true);
+            started = true;
+        }
+        
         
     }
 }
